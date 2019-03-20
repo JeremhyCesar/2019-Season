1102d23929a0fc02ddcef551fadaf381161a3b5a
JeremhyCesar <42977473+JeremhyCesar@users.noreply.github.com>
2019-03-20 14:57:37 -0400

    Update SetArmCommand.java

--- src/main/java/frc/robot/commands/SetArmCommand.java
+++ src/main/java/frc/robot/commands/SetArmCommand.java
@@ -4,7 +4,17 @@ import frc.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+import frc.robot.RobotMap;
+import static frc.robot.RobotMap;
+
 public class SetArmCommand extends Command {
+  
+  double currentAngle = armEncoder.get() / PULSES_PER_REVOLUTION;
+  
+  int error = 0;
+  int P, I, D = 1;
+  int integral, previous_error, setpoint = 0;
+  int rcw = 0;
 
   public SetArmCommand() {
     requires(Robot.armSubsystem);
@@ -15,7 +25,10 @@ public class SetArmCommand extends Command {
   }
   
   protected void execute() {
-  
+     error = DESIRED_PULSES - currentAngle; // Error = Target - Actual
+     this.integral += (error*.02); // Integral is increased by the error*time (which is .02 seconds using normal IterativeRobot)
+     derivative = (error - this.previous_error) / .02;
+     this.rcw = P*error + I*this.integral + D*derivative;
   }
   
   protected boolean isFinished() {
